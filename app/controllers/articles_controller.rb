e5316b8755b83efb89ad4db446b6e4b41eba8a4a
ytruongdang <ytruongdang@gmail.com>
2015-09-04 20:06:38 +0000

    curd

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,43 @@
+class ArticlesController < ApplicationController
+    def new
+        @article = Article.new
+    end
+    def create
+        # render plain: params[:article].inspect
+        @article = Article.new(article_params)
+        
+        @article.save
+        redirect_to @article
+    end
+    def index
+        @articles = Article.all
+    end
+    
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+    end
+    
+    def show
+        @article = Article.find(params[:id])
+    end
+    
+    private
+        def article_params
+            params.require(:article).permit(:title, :text)
+        end
+end
